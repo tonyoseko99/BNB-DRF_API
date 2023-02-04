7d4fea958c6dc4636d87ae55a6a2897d4ee1e719
Tonny Oseko <tonnyoseko0@gmail.com>
2023-02-04 22:40:34 +0300

    create a Location and ListingAmenity models

--- BNB/bnbApp/models.py
+++ BNB/bnbApp/models.py
@@ -51,3 +51,26 @@ class Review(models.Model):
 
     def __str__(self):
         return f'{self.reservation} - {self.rating} stars'
+
+
+class Amenity(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class ListingAmenity(models.Model):
+    listing = models.ForeignKey(
+        Listing, on_delete=models.CASCADE, related_name='amenities')
+    amenity = models.ForeignKey(Amenity, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.listing} - {self.amenity}'
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
