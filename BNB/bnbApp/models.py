07b0005f8e025e38771d47333946aa91e4da4232
Tonny Oseko <tonnyoseko0@gmail.com>
2023-02-04 14:41:01 +0300

    create a User model

--- BNB/bnbApp/models.py
+++ BNB/bnbApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    is_host = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
