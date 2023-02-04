a6d510a37cc8fd46057f4648682ada5bfbbd9d1c
Tonny Oseko <tonnyoseko0@gmail.com>
2023-02-04 14:46:02 +0300

    create a Listing model

--- BNB/bnbApp/models.py
+++ BNB/bnbApp/models.py
@@ -12,3 +12,16 @@ class User(models.Model):
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
+
+
+class Listing(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='listings')
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    location = models.CharField(max_length=100)
+    price_per_night = models.DecimalField(max_digits=7, decimal_places=2)
+    number_of_rooms = models.PositiveSmallIntegerField()
+    number_of_guests = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.title
