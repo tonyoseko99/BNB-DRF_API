c7112ceb7368420fe326b8477b21bc27b2af9180
Tonny Oseko <tonnyoseko0@gmail.com>
2023-02-05 01:09:32 +0300

    view to get listings and a single listing

--- BNB/bnbApp/views.py
+++ BNB/bnbApp/views.py
@@ -20,3 +20,21 @@ def user_detail(request, pk):
     data = {'id': user.id, 'first_name': user.first_name,
             'last_name': user.last_name, 'email': user.email, 'password': user.password, 'is_host': user.is_host}
     return JsonResponse(data)
+
+# get all listings
+
+
+def listing_list(request):
+    listings = Listing.objects.all()
+    data = {'results': [{'id': listing.id, 'owner': listing.owner.id, 'title': listing.title, 'description': listing.description, 'location': listing.location.id,
+                         'price_per_night': listing.price_per_night, 'number_of_rooms': listing.number_of_rooms, 'max_guests': listing.max_guests} for listing in listings]}
+    return JsonResponse(data)
+
+# get a single listing
+
+
+def listing_detail(request, pk):
+    listing = get_object_or_404(Listing, pk=pk)
+    data = {'id': listing.id, 'owner': listing.owner.id, 'title': listing.title, 'description': listing.description, 'location': listing.location.id,
+            'price_per_night': listing.price_per_night, 'number_of_rooms': listing.number_of_rooms, 'max_guests': listing.max_guests}
+    return JsonResponse(data)
