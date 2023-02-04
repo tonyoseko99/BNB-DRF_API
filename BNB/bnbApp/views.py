d117f6112f06b8f56d7056d477b744487b002f2d
Tonny Oseko <tonnyoseko0@gmail.com>
2023-02-05 00:58:36 +0300

    view to get all users

--- BNB/bnbApp/views.py
+++ BNB/bnbApp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from .models import User, Listing, Reservation, Review, Amenity
 
-# Create your views here.
+# get all users
+
+
+def user_list(request):
+    users = User.objects.all()
+    data = {'results': [{'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name,
+                         'email': user.email, 'password': user.password, 'is_host': user.is_host} for user in users]}
